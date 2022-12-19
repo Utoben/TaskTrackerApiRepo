6bf835044ea495b59e1e2546834807b682306716
Utoben <86472697+Utoben@users.noreply.github.com>
2022-12-19 15:24:56 +0500

    Added project with WebApi

---
+++ TaskTrackerApiV1/TaskTrackerApiV1/Models/TaskTrackerApi.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskTrackerApiV1.Models
+{
+    public class TaskTrackerApi
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+
+        public string? Secret { get; set; }
+
+    }
+}
---
+++ TaskTrackerApiV1/TaskTrackerApiV1/Models/TaskTrackerApiContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskTrackerApiV1.Models
+{
+    public class TaskTrackerApiContext : DbContext
+    {
+        public TaskTrackerApiContext(DbContextOptions<TaskTrackerApiContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<TaskTrackerApi> TaskTrackerApiItems { get; set; } = null!;
+    }
+}
